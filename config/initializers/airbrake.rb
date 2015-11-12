a5e1fcdd579640ba6f93557f740e8b4ea4399d45
kokosko <kostya.kokosko@gmail.com>
2015-11-12 13:06:32 +0200

    add airbrake

---
+++ config/initializers/airbrake.rb
@@ -0,0 +1,3 @@
+Airbrake.configure do |config|
+  config.api_key = 'f757528d4e6c1274c7d976df0a09b519'
+end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,5 +2,5 @@ OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   # provider :facebook, Rails.application.secrets['APP_ID'], Rails.application.secrets['APP_SECRET'], { scope: 'user_about_me' }
-  provider :facebook, ENV['APP_ID'], ENV['APP_SECRET']
+  provider :facebook, ENV['APP_ID'], ENV['APP_SECRET'], { scope: 'user_about_me' }
 end
